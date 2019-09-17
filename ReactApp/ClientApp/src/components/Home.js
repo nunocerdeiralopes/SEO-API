fc44c0dba423db5e70f34ac86cee7752da5cb013
Nuno Lopes <nunocerdeiralopes@gmail.com>
2019-09-17 20:58:09 +0100

    Built React form.

--- ReactApp/ClientApp/src/components/Home.js
+++ ReactApp/ClientApp/src/components/Home.js
@@ -1,35 +1,82 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-            <div class="form-row">
-                <div class="form-group col-md-3">
-                    <label for="inputQuery">Query</label>
-                    <input type="text" class="form-control" id="inputQuery" />
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputQuery: '',
+            inputUrl: '',
+            inputCountryDomain: 'co.uk'
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit() {
+
+        fetch('https://localhost:5001/api/google/')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ forecasts: data, loading: false });
+            });
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-row">
+                        <div className="form-group col-md-3">
+                            <label htmlFor="inputQueryID">Query</label>
+                            <input type="text" className="form-control" id="inputQueryID" name="inputQuery" value={this.state.inputQuery} onChange={this.handleInputChange} required />
+                    </div>
+                        <div className="form-group col-md-5">
+                            <label htmlFor="inputUrlID">Url</label>
+                            <input type="text" className="form-control" id="inputUrlID" name="inputUrl" value={this.state.inputUrl} onChange={this.handleInputChange} required />
+                    </div>
+                        <div className="form-group col-md-4">
+                            <label htmlFor="inputCountryDomainID">Country Domain</label>
+                            <select id="inputCountryDomainID" name="inputCountryDomain" value={this.state.inputCountryDomain} className="form-control" onChange={this.handleInputChange} required>
+                            <option value="co.uk">co.uk</option>
+                            <option value="com">com</option>
+                            <option value="co.au">co.au</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group col-md-5">
-                    <label for="inputUrl">Url</label>
-                    <input type="text" class="form-control" id="inputUrl" />
+                    <div className="form-group row">
+                        <div className="col-sm-10">
+                            <button type="submit" className="btn btn-primary">Get Positions</button>
+                    </div>
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="inputCountryDomain">Country Domain</label>
-                    <select id="inputCountryDomain" class="form-control" required>
-                        <option value="co.uk">co.uk</option>
-                        <option value="co.uk">com</option>
-                        <option value="co.au">co.au</option>
-                    </select>
-                </div>
+                </form>
+                <table className='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>Query</th>
+                            <th>Url</th>
+                            <th>Country Domain</th>
+                            <th>Position</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Get Positions</button>
-                </div>
-            </div>
-      </div>
-    );
-  }
+        );
+    }
 }
