bc401ed7ed51483c2509f4a094eb4c054faade7a
Nuno Lopes <nunocerdeiralopes@gmail.com>
2019-09-17 19:31:33 +0100

    Add database.

---
+++ SEO-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using SEO_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEO_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<RecurringKeyword> RecurringKeyword { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["SeoAPIDatabase"].ConnectionString);
+    }
+}
+}
---
+++ SEO-API/Models/RecurringKeyword.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEO_API.Models
+{
+    public class RecurringKeyword
+    {
+    }
+}
